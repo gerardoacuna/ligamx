4d0bf14a8ec2c1672fc39583c05aee5236c03efe
Gerardo Acuña <gerardoacuna@gmail.com>
2012-12-26 18:27:26 -0600

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ligamx::Application.config.secret_token = '157294b077a6220efa9685e8db9638f6668b6f03247742269e719d715a2693d6449edb6b4081567b3042b7625e9901f59213dc8216694b71bb9edb1620ddb910'

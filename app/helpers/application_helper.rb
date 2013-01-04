d4c980a33d4dc2c5c14612c7c2940c02c3596211
Gerardo Acuña <gerardoacuna@gmail.com>
2013-01-04 15:25:04 -0600

    Add Gain/Loss colors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+	
+	def gain_loss(value)
+		if value > 0
+			"gain"
+		elsif value < 0
+			"loss"
+		else
+		end	
+	end
+
+  def bugherd_tag
+  	javascript_tag do
+			"(function (d, t) {
+  var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];
+  bh.type = 'text/javascript';
+  bh.src = '//www.bugherd.com/sidebarv2.js?apikey=gbvl3iqjbjvotksbshxhug';
+  s.parentNode.insertBefore(bh, s);
+  })(document, 'script');"
+  	end  
+  end
+
 end

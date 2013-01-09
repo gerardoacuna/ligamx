f54b9fd0c9840dd81abaca265fd206d60f222ab0
Gerardo Acuña <gerardoacuna@gmail.com>
2013-01-09 00:49:56 -0600

    Add Graph to teams

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,8 +1,9 @@
 module TeamsHelper
 
-	def set_active_to_first(arg, i)
-		css_classes = ['arg']
+	def set_active_to_first(i)
+		css_classes = ['tab']
     css_classes << 'active' if i == 0
     css_classes
 	end
+	
 end
